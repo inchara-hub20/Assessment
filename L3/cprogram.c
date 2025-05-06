043ce198b932ff683ace698b26b69afec6c3851d
inchara-hub20 <inchara.m@s.amity.edu>
2025-05-06 18:44:44 +0530

    Delete L3/cprogram.c
    delete message

--- L3/cprogram.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- void main () {
-         int n;
-         printf("1.B.tech CSE");
-         if(n==1)
-         {
-                 printf("you have choosen B.tech CSE");
-         }
- }
